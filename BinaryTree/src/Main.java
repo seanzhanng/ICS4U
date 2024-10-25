ca472fdf4395e450528bef26e450cfd0ed8230b6
Sean <snyoulin@gmail.com>
2024-10-24 22:31:45 -0400

    Tested MyBinaryTree.java file

--- BinaryTree/src/Main.java
+++ BinaryTree/src/Main.java
@@ -2,15 +2,12 @@ public class Main {
     public static void main(String[] args) {
         MyBinaryTree theTree = new MyBinaryTree();
 
-        StudentInfo s1 = new StudentInfo(1, "sean");
-        StudentInfo s2 = new StudentInfo(2, "sean");
-        StudentInfo s3 = new StudentInfo(3, "sean");
-        StudentInfo s4 = new StudentInfo(4, "sean");
-        StudentInfo s5 = new StudentInfo(5, "sean");
-        StudentInfo s6 = new StudentInfo(6, "sean");
-        StudentInfo s7 = new StudentInfo(7, "sean");
-        StudentInfo s8 = new StudentInfo(8, "sean");
-
+        StudentInfo s1 = new StudentInfo(8, "sean");
+        StudentInfo s2 = new StudentInfo(3, "sean");
+        StudentInfo s3 = new StudentInfo(10, "sean");
+        StudentInfo s4 = new StudentInfo(1, "sean");
+        StudentInfo s5 = new StudentInfo(6, "sean");
+        StudentInfo s6 = new StudentInfo(14, "sean");
 
         theTree.preorder(theTree.root);
         theTree.postorder(theTree.root);
@@ -22,8 +19,6 @@ public class Main {
         theTree.addToTree(theTree.root, s4);
         theTree.addToTree(theTree.root, s5);
         theTree.addToTree(theTree.root, s6);
-        theTree.addToTree(theTree.root, s7);
-        theTree.addToTree(theTree.root, s8);
 
         theTree.preorder(theTree.root);
         System.out.println();
@@ -33,5 +28,6 @@ public class Main {
         System.out.println();
 
         System.out.println(theTree.num);
+
     }
 }
