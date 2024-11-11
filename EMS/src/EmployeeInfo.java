2357dd67b682935d89202b9feaf45d92f2f80860
Sean <snyoulin@gmail.com>
2024-11-11 10:47:46 -0500

    Changed project name

---
+++ EMS/src/EmployeeInfo.java
@@ -0,0 +1,8 @@
+public class EmployeeInfo {
+    public int empNum;
+    public String firstName;
+    public String lastName;
+    public int gender;
+    public String workLoc;
+    public double deductRate;
+}
---
+++ EMS/src/PTE.java
@@ -0,0 +1,5 @@
+public class PTE extends EmployeeInfo {
+    public double hourlyWage;
+    public double hoursPerWeek;
+    public double weeksPerYear;
+}
