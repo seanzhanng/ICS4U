775f949696e77207bdd1509cb612a158938db869
Sean <snyoulin@gmail.com>
2024-12-31 23:48:02 -0500

    Final project

---
+++ Final Project/src/Cell.java
@@ -0,0 +1,55 @@
+public class Cell {
+    private int x, y;
+    private boolean visited;
+    private boolean[] walls;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.visited = false;
+        this.walls = new boolean[4];
+        for (int i = 0; i < 4; i++) {
+            walls[i] = true;
+        }
+        System.out.println("Cell created at (" + x + ", " + y + ") with all walls intact.");
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public boolean[] getWalls() {
+        return walls;
+    }
+
+    public void removeWall(int direction, Cell neighbor) {
+        // Remove a wall by index (0=top, 1=right, 2=bottom, 3=left)
+
+        walls[direction] = false;
+
+        if (direction == 0) {
+            neighbor.walls[2] = false;
+        }
+        else if (direction == 1) {
+            neighbor.walls[3] = false;
+        }
+        else if (direction == 2) {
+            neighbor.walls[0] = false;
+        }
+        else {
+            neighbor.walls[1] = false;
+        }
+    }
+}
---
+++ Final Project/src/Grid.java
@@ -0,0 +1,21 @@
+public class Grid {
+    private Cell[][] grid;
+
+    public Grid(int width, int height) {
+        grid = new Cell[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                grid[x][y] = new Cell(x, y);
+            }
+        }
+        System.out.println("Grid created with width: " + width + " and height: " + height);
+    }
+
+    public Cell getCell(int x, int y) {
+        return grid[x][y];
+    }
+
+    public boolean isValidMove(int x, int y) {
+        return x >= 0 && y >= 0 && x < grid.length && y < grid[0].length; // return true as long as in bounds
+    }
+}
